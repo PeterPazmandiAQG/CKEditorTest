8bc82e3ab5a6f1926bf648e54a6c9eb7b131366d
inspire-coding <inspire.coding.apps@gmail.com>
2021-10-20 14:27:08 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,118 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
+import * as customBuild from './build/build/ckeditor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'CKEditorTest';
+export class AppComponent implements OnInit, ControlValueAccessor {
+  public Editor = customBuild;
+  @Input() readonly: boolean = false;
+
+  _value: string = '';
+
+  get value() {
+    return this._value;
+  }
+
+  set value(v: string) {
+    if (v !== this._value) {
+      this._value = v;
+      console.log(this._value);
+    }
+  }
+
+  constructor() { }
+  onTouch() { 
+  }
+
+  writeValue(obj: any): void {
+    this._value = obj;
+  }
+  registerOnChange(fn: any): void {
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  title = 'ckeditorAngular10';
+  @Input() config = {
+    toolbar: {
+      items: [
+        'heading', '|',
+        'fontfamily', 'fontsize',
+        'alignment',
+        'fontColor', 'fontBackgroundColor', '|',
+        'bold', 'italic', 'strikethrough', 'underline', 'subscript', 'superscript', '|',
+        'link', '|',
+        'outdent', 'indent', '|',
+        'bulletedList', '-', 'numberedList', 'todoList', '|',
+        'code', 'codeBlock', '|',
+        'insertTable', '|',
+        'imageUpload', 'blockQuote', '|',
+        'todoList'
+        ,
+        'undo', 'redo',
+      ],
+      shouldNotGroupWhenFull: true,
+
+    },
+    image: {
+      // Configure the available styles.
+      styles: [
+        'alignLeft', 'alignCenter', 'alignRight'
+      ],
+
+      // Configure the available image resize options.
+      resizeOptions: [
+        {
+          name: 'resizeImage:original',
+          label: 'Original',
+          value: null
+        },
+        {
+          name: 'resizeImage:50',
+          label: '25%',
+          value: '25'
+        },
+        {
+          name: 'resizeImage:50',
+          label: '50%',
+          value: '50'
+        },
+        {
+          name: 'resizeImage:75',
+          label: '75%',
+          value: '75'
+        }
+      ],
+
+      // You need to configure the image toolbar, too, so it shows the new style
+      // buttons as well as the resize buttons.
+      toolbar: [
+        'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
+        '|',
+        'ImageResize',
+        '|',
+        'imageTextAlternative'
+      ]
+    },
+    // simpleUpload: {
+    //    The URL that the images are uploaded to.
+    uploadUrl: 'http://localhost:52536/api/Image/ImageUpload',
+
+    //   Enable the XMLHttpRequest.withCredentials property.
+
+    //},
+
+    language: 'en'
+  };
+
+  
 }
